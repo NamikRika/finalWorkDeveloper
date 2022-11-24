e1a017da77b20cf1022dfb4ab0e09bc429a9f81c
NamikR <uchitelnamik@mail.ru>
2022-11-24 11:49:35 +0400

    написан код запроса размера массива и текта от пользовтаеля

--- itogSpec.cs
+++ itogSpec.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [] FillingStringArray (string sizeArray)
+{
+    string userText = "";
+     int convertSizeArray = Convert.ToInt32(sizeArray);
+      
+     string [] stringArray = new string [convertSizeArray];
+
+       for (int i=0; i < convertSizeArray ; i++)
+       {
+        
+           Console.Write("Введите слово ");
+           userText =  Convert.ToString(Console.ReadLine());
+           stringArray[i] = userText;
+        
+       }
+     
+          return stringArray;
+    
+}
+
+Console.Write("Введите размер массива ");
+string sizeArray = Convert.ToString(Console.ReadLine());
+FillingStringArray(sizeArray);
